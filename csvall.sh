4066a9c9e622c935153a8df07fd393949c88dc1e
John <ebusd@ebusd.eu>
2024-07-06 18:14:30 +0200

    do not touch symlinks

--- csvall.sh
+++ csvall.sh
@@ -2,12 +2,12 @@
 sed -i \
   -e 's/^id,/#id,/' \
   latest/en/_templates.csv latest/de/_templates.csv \
-&& sed -i \
+&& find latest/ -mindepth 3 -type f -not -name "_templates.csv" -exec sed -i \
   -e 's#\(HcName.*\),STR:11,#\1,hcname,#i' \
   -e 's#\(phone.*\),STR:9,#\1,phone,#i' \
   -e 's#,UCH,hour,#,hours,,#i' \
   -e 's#,ULG,hour,#,hoursum,,#i' \
-  latest/en/vaillant/*.*.csv latest/de/vaillant/*.*.csv latest/en/vaillant/*.inc latest/de/vaillant/*.inc \
+  \{\} \; \
 && npm run csv2tsp \
 && npm run csv2tsp-combine \
 && mv outtsp.de/i18n.yaml outtsp/i18n.yaml \
