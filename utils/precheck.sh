2301e47ebc0e2a1523bc35c8525cf807e015a24c
Petr Vraník <Petr.Vranik@mgm-tp.com>
2025-10-14 12:18:06 +0200

    extend Czech translation handling

--- utils/precheck.sh
+++ utils/precheck.sh
@@ -7,12 +7,15 @@ rm -rf outcsv*
 npm run compile
 mv outcsv/@ebusd/ebus-typespec outcsv/en
 mv outcsv.de/@ebusd/ebus-typespec outcsv/de
-rm -rf outcsv/@ebusd outcsv.de
-find src -type l -exec ./utils/copylink.sh src \{\} outcsv/de outcsv/en \;
+mv outcsv.cs/@ebusd/ebus-typespec outcsv/cs
+rm -rf outcsv/@ebusd outcsv.de outcsv.cs
+find src -type l -exec ./utils/copylink.sh src \{\} outcsv/de outcsv/en  \;
+find src -type l -exec ./utils/copylink.sh src \{\} outcsv/cs outcsv/en  \;
 if [[ "$(realpath outcsv)" != "$targetdir" ]]; then
   echo "moving folders to $targetdir"
   mkdir -p "$targetdir/"
   mv outcsv/en "$targetdir/"
   mv outcsv/de "$targetdir/"
+  mv outcsv/cs "$targetdir/"
   rm -rf outcsv
 fi
